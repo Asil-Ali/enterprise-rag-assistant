43891644d6af38c737459fe02b31232b98988ee3
Asil-Ali <aliaaeel125@gmail.com>
2026-02-14 21:35:23 +0200

    Create loader.py

---
+++ agents/rag/loader.py
@@ -0,0 +1,19 @@
+from langchain.document_loaders import PyPDFLoader, TextLoader
+import tempfile
+
+def load_documents(files):
+    documents = []
+
+    for file in files:
+        suffix = file.name.split(".")[-1]
+        with tempfile.NamedTemporaryFile(delete=False, suffix=f".{suffix}") as tmp:
+            tmp.write(file.read())
+            path = tmp.name
+
+        if suffix.lower() == "pdf":
+            loader = PyPDFLoader(path)
+            documents.extend(loader.load())
+        else:
+            loader = TextLoader(path)
+            documents.extend(loader.load())
+    return documents
