9cb8533b89f8eea6762a25861b3e1a6c40b69b2e
Asil-Ali <aliaaeel125@gmail.com>
2026-02-14 21:28:28 +0200

    Create qa_agent.py

---
+++ agents/qa_agent.py
@@ -0,0 +1,28 @@
+from langchain.chat_models import ChatOpenAI
+from rag.retriever import retrieve_context
+import os
+
+def answer_with_rag(question, vectordb):
+    context = retrieve_context(question, vectordb)
+
+    llm = ChatOpenAI(
+        model="openai/gpt-4o-mini",
+        openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+        openai_api_base="https://openrouter.ai/api/v1"
+    )
+
+    prompt = f"""
+Answer strictly from the context below.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Return a structured and clear answer.
+"""
+
+    answer = llm.predict(prompt)
+
+    return {"answer": answer, "context_used": context}
