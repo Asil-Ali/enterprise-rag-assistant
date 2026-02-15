380463cbd632ef95edc398c19f69b8d374cae3b1
Asil-Ali <aliaaeel125@gmail.com>
2026-02-15 11:43:23 +0200

    Update vectorstore.py

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -1,5 +1,5 @@
 from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 import os
 
 def build_vectorstore(documents):
